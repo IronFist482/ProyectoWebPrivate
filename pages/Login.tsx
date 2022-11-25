536479de651c671f82520451b4bb00864e96a361
IronFist482 <icernap2000@alumno.ipn.mx>
2022-11-25 13:11:30 -0600

    Update Login.tsx

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -22,9 +22,9 @@ export default function Login(){
         <div className="texto">Iniciar Sesion</div>
         <form className="formLogin" method='post' onSubmit={validacion}>
           <label className='labelCorreo'>Correo</label>
-          <input type="text" className='label00' placeholder="ejemplo@gmail.com" onInput={event => setCorreo(event.target.value)} required/>
+          <input type="text" className='label00' placeholder="ejemplo@gmail.com" onChange={event => setCorreo(event.target.value)} required/>
           <label className='labelContrasena'>Contraseña</label>
-          <input type="password" className='label01' placeholder="contraseña1234" onInput={event => setContrasena(event.target.value)} required/>
+          <input type="password" className='label01' placeholder="contraseña1234" onChange={event => setContrasena(event.target.value)} required/>
           <button className="loginBoton2 tipoLetra">Iniciar</button>
         </form>
         <div className="logo-normal"></div>
